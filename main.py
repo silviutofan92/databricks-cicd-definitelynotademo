d95bd51eb42a078a6183cfff4d48439dc1326414
Silviu Tofan <silviu.tofan@databricks.com>
2020-06-25 22:24:15 +0100

    testing dbconnect code

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ spark = SparkSession\
 
 print("Testing simple count")
 
-# The Spark code will execute on the Azure Databricks cluster...
-print(spark.range(100).count())
+def spark_f(n_rows):
+    # The Spark code will execute on the Azure Databricks cluster...
+    n = spark.range(n_rows).count()
+    return n
 
+def python_f(n_rows):
+    n = len(range(n_rows))
+    return n
+
+spark_count = spark_f(10000)
+python_count = python_f(9999)
+
+print(spark_count)
+print(python_count)
